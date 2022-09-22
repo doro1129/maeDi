58f4a5648402930565bb12df154ed339323e78dd
YeJun, Jung <yejun614@naver.com>
2022-09-22 19:34:29 +0900

    Rename canvas scaler setter

--- Summer Project/Assets/Scripts/UI/Setter/CanvasScalerSetter.cs
+++ Summer Project/Assets/Scripts/UI/Setter/CanvasScalerSetter.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 /// <summary>
 /// Canvas scaler setter for screen resolution option
 /// </summary>
-public class SetterCanvasScaler : MonoBehaviour
+public class CanvasScalerSetter : MonoBehaviour
 {
     /// <summary>
     /// Target canvas scaler
